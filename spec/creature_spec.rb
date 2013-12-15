660e7e652b99beb27093ae8fc7702ab2c3ddd98d
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-15 15:56:59 -0600

    Remove unnecessary test

--- spec/creature_spec.rb
+++ spec/creature_spec.rb
@@ -13,10 +13,6 @@ describe Creature do
 		context "when 'alive' is not specified" do
 			let(:creature) { Creature.new(2, 5) }
 
-			it "does not raise an error" do
-				expect(creature).not_to raise_error
-			end
-
 			it "is false" do
 				expect(creature.alive).to be_false
 			end
