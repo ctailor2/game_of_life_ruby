3d0633210cc0cf23770c56795592b4fc485447e6
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-15 19:02:58 -0600

    Start game tick actions

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 class Game
-	attr_reader :creatures, :tick_count
+	attr_reader :creatures
+	attr_accessor :tick_count
 
 	def initialize(num_creatures)
 		@creatures = []
@@ -21,5 +22,35 @@ class Game
 
 	def starting_bound(num_creatures)
 		Math.sqrt(num_creatures * 5).floor.to_i
+	end
+
+	def tick
+		increment_counter
+		build_neighborhood
+	end
+
+	def increment_counter
+		self.tick_count += 1
+	end
+
+	def vacant_lots
+		lots = []
+		creatures.each do |creature|
+			lots += creature.neighborhood
+		end
+		lots.reject do |location|
+			live_creature_locations.include?(location)
+		end
+		lots
+	end
+
+	def build_neighborhood
+		vacant_lots.each do |location|
+			creatures << Creature.new(*location)
+		end
+	end
+
+	def live_creature_locations
+		creatures.select{ |creature| creature.alive }.map{ |creature| creature.location }
 	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,9 @@
 require_relative '../game'
 
 describe Game do
+	let(:game) { Game.new(5) }	
+
 	describe "#creatures" do
-		let(:game) { Game.new(5) }
 
 		it "is a collection" do
 			expect(game.creatures).to be_a(Array)
@@ -18,7 +19,6 @@ describe Game do
 	end
 
 	describe "#starting_locations" do
-		let(:game) { Game.new(5) }
 
 		it "returns the correct number of locations" do
 			expect(game.starting_locations(10).length).to eq(10)
@@ -30,19 +30,76 @@ describe Game do
 	end
 
 	describe "#starting_bound" do
-		let(:game) { Game.new(6) }
-
 		it "ensures creatures take up at least 20% of their starting area" do
 			num_creatures = game.creatures.length
-			expect(num_creatures.to_f / (game.starting_bound(6) ** 2)).to be >= 0.20
+			expect(num_creatures.to_f / (game.starting_bound(5) ** 2)).to be >= 0.20
 		end
 	end
 
 	describe "#tick_count" do
-		let(:game) { Game.new(5) }
 
 		it "starts at 0" do
 			expect(game.tick_count).to eq(0)
+		end
+	end
+
+	describe "#tick" do
+
+		context "with 1 live creature" do
+			it "populates the correct surrounding spots with creatures" do
+				game.creatures.clear
+				creature = Creature.new(1, 1, true)
+				game.creatures << creature
+				game.tick
+				game.creatures.delete(creature)
+				filled_locations = game.creatures.map{ |creature| creature.location }
+				expect(filled_locations).to include([0, 0], [1, 0], [2, 0], [0, 1], [2, 1], [0, 2], [1, 2], [2, 2])
+			end
+
+			it "populates vacancies with dead creatures" do
+				game.creatures.clear
+				creature = Creature.new(1, 1, true)
+				game.creatures << creature
+				game.tick
+				game.creatures.delete(creature)
+				expect(game.creatures.all?{ |creature| creature.alive == false }).to be_true
+			end
+		end
+
+		context "with 2 adjacent live creatures" do
+			it "populates the correct surrounding spots with dead creatures" do
+				game.creatures.clear
+				creature_a = Creature.new(1, 1, true)
+				creature_b = Creature.new(2, 1, true)
+				game.creatures << creature_a
+				game.creatures << creature_b
+				game.tick
+				game.creatures.delete(creature_a)
+				game.creatures.delete(creature_b)
+				filled_locations = game.creatures.map{ |creature| creature.location }
+				expect(filled_locations).to include([0, 0], [1, 0], [2, 0], [3, 0], [0, 1], [3, 1], [0, 2], [1, 2], [2, 2], [3, 2])				
+			end
+		end
+
+		context "with 2 diagonally adjacent live creatures" do
+			it "populates the correct surrounding spots with dead creatures" do
+				game.creatures.clear
+				creature_a = Creature.new(1, 1, true)
+				creature_b = Creature.new(2, 2, true)
+				game.creatures << creature_a
+				game.creatures << creature_b
+				game.tick
+				game.creatures.delete(creature_a)
+				game.creatures.delete(creature_b)
+				filled_locations = game.creatures.map{ |creature| creature.location }
+				expect(filled_locations).to include([0, 0], [1, 0], [2, 0], [0, 1], [2, 1], [3, 1], [0, 2], [1, 2], [3, 2], [1, 3], [2, 3], [3, 3])				
+			end
+		end
+	end
+
+	describe "#increment_counter" do
+		it "increments tick_count" do
+			expect{ game.tick }.to change{ game.tick_count }.from(0).to(1)			
 		end
 	end
 end
